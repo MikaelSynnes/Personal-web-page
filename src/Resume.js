ef8a0c5345ad706091fb7d5320d231aec2fae28c
Mikael <Mikaelsynnes@live.no>
2018-07-09 19:00:27 +0200

    Fixed spelling errors

--- src/Resume.js
+++ src/Resume.js
@@ -4,11 +4,12 @@ class Resume extends Component {
   render() {
     return (
       <div id="resume">
-      <h2>CV</h2>
+      <h2>Curriculum Vitae</h2>
+      <p></p>
         <div id="info">E-mail: Mikaelsynnes@live.no</div>
         <div id="info">Telephone: +4793894003</div>
         <div id="info">Birth: 07.01.1994</div>
-        <div id="info">Adress: Vervgata 1 6003 Ålesund Norway</div>
+        <div id="info">Address: Vervgata 1 6003 Ålesund Norway</div>
 
       <h3>Education</h3>
         <div id="info"><b>2015-2018</b>   Bachelor Computer Engineering NTNU Ålesund</div>
@@ -22,7 +23,7 @@ class Resume extends Component {
 
       <h3>Technology</h3>
 
-      <div id="info"> <b>Programming: </b> Java, js, Reactjs, C3, Android(java), php, sql, web</div>
+      <div id="info"> <b>Programming: </b> Java, js, Reactjs, C#, Android(java), php, sql, web</div>
       <div id="info"><b>Version control: </b>  Git</div>
       <div id="info"><b>Tools: </b> Jira and Confluence</div>
 
