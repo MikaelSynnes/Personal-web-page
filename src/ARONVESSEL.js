d7a7e42aec091506f8d5ca25b6a5a93818c2c855
Mikael <Mikaelsynnes@live.no>
2018-07-13 22:33:15 +0200

    - Fixed images for hinder. - Added github links inside projects

--- src/ARONVESSEL.js
+++ src/ARONVESSEL.js
@@ -13,7 +13,8 @@ class ARONVESSEL extends Component {
         <p>This project was part of the bachelor thesis in computer engineering at NTNU Ålesund. The demoapplication was created using Unity3D for
             use with Microsoft Hololens. The purpose for the application was to show interview candidates for the thesis how the possible outcome of an
             Augmented Reality application would look like.
-            Pictures below are taking of the demo within unity and from the hololens during demo.
+            Pictures below are taking of the demo within unity and from the hololens during demo.<br></br> 
+            <a href="https://github.com/MikaelSynnes/AR-on-vessel-Bridge">GitHub link</a>
         </p>
         <img src= "https://i.imgur.com/JywGyW3.png"/>
       <img src= "https://i.imgur.com/8ds510V.jpg"/>
--- src/Hinder.js
+++ src/Hinder.js
@@ -12,19 +12,23 @@ class Hinder extends Component {
         <p>Hinder is an chat application made for use by android smartphones. The project is a group project related to a school assignment for mobile
           and distributed applications. The backend is created using RESTApi and Java, the frontend is created in Android studios with Java.
           The application uses gps coordinates from the phone to locate users closeby and listing them for the user to chose who he wants to talk with.
+          When a chat has been made, it can be found in a new list so ti will never get lost, even if the person gets out of range.
+          </p>
+
+          <p>
+          Images below are taken during screenshots from the application. (to many decimals, but didn't have newer screenshots)
+          
 
         
         </p>
-        <img src="" alt="To be added" />
-         <p>
-
-            
-
-
-
-        </p>
- 
-        <p></p>
+        <a href="https://github.com/MikaelSynnes/Hinder" >Github backend</a> <br />
+      
+        <a href="https://github.com/howardie/HinderAndroid1">Github frontend</a>
+           <div id="hinder">  
+           <p />
+            <img src="https://i.imgur.com/JTPDxnI.png" alt="chat" />
+            <img src="https://i.imgur.com/ETJDVrt.png" alt="Dummy data for app"/>
+           </div>
         </article>
       </div>
     );
